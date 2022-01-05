2f25a8c93ed7fd1b1b8847b0009186aa527a61ce
aratrikaray21 <91767112+aratrikaray21@users.noreply.github.com>
2022-01-05 20:45:42 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,6 @@
+#Displaying "Hello World!" on the screen.
+#Giving user basic introduction.
+#print is a Python defined built-in function.
+
+print("Hello World!")
+print("Hello Everyone! This user's name is Aratrika Ray!")
